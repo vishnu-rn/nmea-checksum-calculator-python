1f5fc2d480e2d2572f402c8b7c3f3dd1d6c712d5
vishnu-rn <50511699+vishnu-rn@users.noreply.github.com>
2023-09-02 08:42:06 +0530

    Update checksumCalc.py

--- checksumCalc.py
+++ checksumCalc.py
@@ -2,25 +2,24 @@ from time import perf_counter_ns
 
 
 def validator(nmea_sentence: str | None = None):
-    if nmea_sentence is not None:
-        # Split the sentence and provided checksum value
-        chk_str, chk_hex = nmea_sentence.split("*")
-        # Convert the provided checksum into hex notation
-        chk_hex = '0x' + chk_hex
-        # Initialize a variable for byte-wise XOR of sentence characters
-        chk_str_hex = 0
-        # Remove the '$' at the beginning of each sentence
-        for char in chk_str[1:]:
-            chk_str_hex ^= ord(char)
-        chk_str_hex = hex(chk_str_hex)
+    if not len(row) or row[0] != '$':
+        return False
 
-        if chk_str_hex == chk_hex:
-            return True
-
+    chksum = 0x0
+    splitRow = row.split('*')
+    for c in splitRow[0]:
+        if c == ',' or c == '$':
+            continue
+        else:
+            # print(f'{bin(chksum)} ^ {bin(ord(c))} = {bin(chksum^ord(c))}')
+            chksum ^= ord(c)
+    if hex(chksum)[2:].upper() == splitRow[1]:
+        return True
+    # print(chksum)
     return False
 
-
 if __name__ == '__main__':
+
     check_str = "$GPGGA,121315.00,0128.96496,F,02330.13368,L,1,08,0.94,31.7,M,-94.3,M,,*49"
 
     start = perf_counter_ns()
